d216f08243e86800c016ca04ad197e40afdac0cc
Johannes Charra <Johannes.Charra@gmx.de>
2011-06-24 00:07:05 +0200

    implemented BF interpreter

--- ook.py
+++ ook.py
@@ -16,20 +16,40 @@ class OokParser(object):
                            ('ook!', 'ook.'): 'write',
                            ('ook.', 'ook!'): 'read'}
                            
-    def parse(self, items):
+    def parse(self, input_text):
+        items = input_text.lower().split()
         for i in range(0, len(items), 2):
-            yield items[i], items[i+1]
+            x = (items[i], items[i+1])
+            if x in self.primitives or x in (self.BEGIN, self.END):
+                yield x
+
+class BrainfuckParser(object):
+    def __init__(self):
+        self.BEGIN = '['
+        self.END = ']'
+        self.primitives = {'+': 'inc',
+                           '-': 'dec',
+                           '>': 'right',
+                           '<': 'left',
+                           '.': 'write',
+                           ',': 'read'}
+                           
+    def parse(self, input_text):
+        for x in input_text:
+            if x in self.primitives or x in (self.BEGIN, self.END):
+                yield x
 
 class Interpreter(object):
-    def __init__(self, parser):
-        self.set_parser(parser)
+    def __init__(self, ook_mode=True):
+        self.bf_parser = BrainfuckParser()
+        self.ook_parser = OokParser()
+        self.set_parser(ook_mode and self.ook_parser or self.bf_parser)
 
     def reset(self):
         self.cells = deque([0])
         self.index = 0
         self.input_buffer = []
         self.output_buffer = []
-        self.recording = False
         self.open_loops = 0
         self.loop = []
 
@@ -67,7 +87,7 @@ class Interpreter(object):
         self.reset()
 
     def interpret_raw_text(self, text):
-        self.input_buffer.extend(text.lower().split())
+        self.input_buffer.extend(self.parser.parse(text))
         try:
             self.interpret_items(self.input_buffer)
             self.input_buffer = []
@@ -78,44 +98,55 @@ class Interpreter(object):
             sys.exit(1)
 
     def interpret_items(self, items):
-        for elem in self.parser.parse(items):
-            if self.recording:
-                if elem == self.parser.END:
-                    self.open_loops -= 1
-                    if self.open_loops < 0:
-                        raise ValueError("End without begin")
-                    elif self.open_loops == 0:
-                        self.recording = False
-                        while self.cells[self.index]:
-                            self.interpret_items(self.loop)
-                        continue
-                elif elem == self.parser.BEGIN:
-                    self.open_loops += 1
-                self.loop.extend(elem)
+        for item in items:
+            if self.open_loops:
+                self.interpret_inside_loop(item)
             else:
-                if elem == self.parser.END:
-                    raise ValueError("End without begin")
-                elif elem == self.parser.BEGIN:
-                    self.recording = True
-                    self.open_loops = 1
-                    self.loop = []
-                elif elem in self.parser.primitives:
-                    method = self.parser.primitives[elem]
-                    getattr(self, method)()
-                else:
-                    raise ValueError("Unknown token %s" % (elem, ))
+                self.interpret_directly(item)
+
+    def interpret_inside_loop(self, item):
+        if item == self.parser.END:
+            self.open_loops -= 1
+            if self.open_loops < 0:
+                raise ValueError("End without begin")
+            elif self.open_loops == 0:
+                while self.cells[self.index]:
+                    self.interpret_items(self.loop)
+                return
+        elif item == self.parser.BEGIN:
+                self.open_loops += 1
+        self.loop.append(item)
+
+    def interpret_directly(self, item):
+        if item == self.parser.END:
+            raise ValueError("End without begin")
+        elif item == self.parser.BEGIN:
+            self.open_loops = 1
+            self.loop = []
+        elif item in self.parser.primitives:
+            method = self.parser.primitives[item]
+            getattr(self, method)()
+        else:
+            print "Unknown token '%s' - ignored" % (item, )
 
     def interpret_file(self, fname):
         file = open(fname, 'r')
         self.interpret_raw_text(file.read())
         
     def interactive_mode(self):
-        print "Ook! interpreter V0.9 - written by Johannes Charra in 2011."
-        print "Type '?' to display the status of the Ook! machine. Empty input quits."
+        print "Ook! and Brainfuck interpreter V0.9 - written by Johannes Charra in 2011."
+        print "Type '?' to display the status of the interpreter. "
+        print "Type 'b' to enter brainfuck mode. Empty input quits."
         while True:
             inp = raw_input("oo> ").strip()
             if inp == "?":
                 print self
+            elif inp == "b":
+                print "Entering brainfuck mode. Type 'o' to return to Ook!"
+                self.set_parser(self.bf_parser)
+            elif inp == "o":
+                print "Entering Ook! mode. Type 'b' to return to brainfuck."
+                self.set_parser(self.ook_parser)
             elif inp == "":
                 print self
                 break
@@ -134,7 +165,7 @@ class Interpreter(object):
                       self.as_ascii())
         
 if __name__ == '__main__':
-    ook = Interpreter(parser=OokParser())
+    ook = Interpreter()
     if len(sys.argv) < 2:
         ook.interactive_mode()
     else:
